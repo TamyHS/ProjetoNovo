5f3f4c9935056dd01d023579e165325b6d15b0c9
unknown <thisphp@gmail.com>
2020-10-20 17:57:13 -0300

    Incluindo alterção para a aula de configuração de ambeinte

--- src/main/java/io/github/tamyhs/MinhaConfiguration.java
+++ src/main/java/io/github/tamyhs/MinhaConfiguration.java
@@ -1,14 +1,27 @@
 package io.github.tamyhs;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+//Para configurar essa configuração para um ambiente especifico
+@Profile("development")
 public class MinhaConfiguration {
 
-    @Bean(name = "applicationName")
+    @Bean
+    public CommandLineRunner executar(){
+        //Pode colocar todos os argumentos que quiser
+        return  args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
+    }
+
+    //Criação da configuração da Bean
+    /*@Bean(name = "applicationName")
     public String applicationName() {
         return "Sistema de Vendas";
-    }
+    }*/
 }
 
