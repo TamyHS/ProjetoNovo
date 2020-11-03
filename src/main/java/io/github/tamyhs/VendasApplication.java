3dfc3bf8a10bb0ede9bc35033cfb90c79287fc10
unknown <thisphp@gmail.com>
2020-11-03 18:34:27 -0300

    Inclusão da aula de inserindo os dados e trazendo eles

--- src/main/java/io/github/tamyhs/VendasApplication.java
+++ src/main/java/io/github/tamyhs/VendasApplication.java
@@ -1,5 +1,7 @@
 package io.github.tamyhs;
 
+import io.github.tamyhs.domain.entity.Cliente;
+import io.github.tamyhs.domain.repositorio.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -29,9 +33,20 @@ public class VendasApplication {
         };
     }*/
 
-    @GetMapping("/hello")
+    /*@GetMapping("/hello")
     public String helloWorld(){
         return applicationName;
+    }*/
+
+    @Bean
+    public CommandLineRunner init (@Autowired Clientes clientes){
+        return args -> {
+            clientes.salvar(new Cliente("Tamy"));
+            clientes.salvar(new Cliente("Outro cliente"));
+
+            List<Cliente> todosClientes = clientes.obertTodos();
+            todosClientes.forEach(System.out::println);
+        };
     }
 
     public static void main(String[] args) {
