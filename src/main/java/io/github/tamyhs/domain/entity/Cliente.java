df2ed3d0228ad0e99af8dd0467285ac5a2f4af8a
unknown <thisphp@gmail.com>
2020-11-10 17:53:19 -0300

    Aprendendo sobre JPA.

--- src/main/java/io/github/tamyhs/domain/entity/Cliente.java
+++ src/main/java/io/github/tamyhs/domain/entity/Cliente.java
@@ -1,8 +1,18 @@
 package io.github.tamyhs.domain.entity;
 
+import javax.persistence.*;
+
+@Entity
+//Para fazer a definição da tabela
+@Table(name = "cliente")
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "nome", length = 100)
     private String nome;
 
     public Cliente(){
