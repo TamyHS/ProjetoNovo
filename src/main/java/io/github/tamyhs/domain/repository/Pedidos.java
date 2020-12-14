779cb7de0c90f667971e57c393bd3e084bf6361a
unknown <thisphp@gmail.com>
2020-12-14 18:41:30 -0300

    Aula para cadastrar o pedido para o cliente.

--- src/main/java/io/github/tamyhs/domain/repository/Pedidos.java
+++ src/main/java/io/github/tamyhs/domain/repository/Pedidos.java
@@ -1,7 +1,12 @@
 package io.github.tamyhs.domain.repository;
 
+import io.github.tamyhs.domain.entity.Cliente;
 import io.github.tamyhs.domain.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Pedidos extends JpaRepository<Pedido, Integer> {
+
+    List<Pedido> findByCliente(Cliente cliente);
 }
