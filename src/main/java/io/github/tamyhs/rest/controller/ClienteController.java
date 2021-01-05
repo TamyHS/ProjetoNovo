6c4cdfab707552c728f1f31b858690a332ee1671
unknown <thisphp@gmail.com>
2021-01-04 22:31:35 -0300

    Começo da aula de rest.

--- src/main/java/io/github/tamyhs/rest/controller/ClienteController.java
+++ src/main/java/io/github/tamyhs/rest/controller/ClienteController.java
@@ -1,18 +1,24 @@
 package io.github.tamyhs.rest.controller;
 
+import io.github.tamyhs.domain.entity.Cliente;
+import io.github.tamyhs.domain.repository.Clientes;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(value = "hello/{nome}", method = RequestMethod.GET)
+    private Clientes clientes;
+
+    public ClienteController(Clientes clientes){
+        this.clientes = clientes;
+    }
+
+    @GetMapping("/api/clientes/{id}")
     @ResponseBody
-    public String helloClientes( @PathVariable("nome") String nomeCliente ){
-        return String.format("Hello %s ", nomeCliente);
+    public ResponseEntity<Cliente> getClienteById( @PathVariable Integer id ){
+
     }
 }
